e8ddbc00c6b7d344f913e339537a284d1133f2a5
Ryan Rix <ry@n.rix.si>
2012-03-23 15:29:31 -0700

    Fix spec to not be broken
    Why are we using FactoryGirl to generate a model test object?

--- spec/models/user_action_spec.rb
+++ spec/models/user_action_spec.rb
@@ -1,37 +1,36 @@
 require 'spec_helper'
 
 describe UserAction do
-  let (:user)   {Factory :user                           }
-  let (:tool)   {Factory :tool                           }
-  let (:action) {Factory :user_action, user:   user,
-                                       event:  'created' }
+  let (:user) { Factory :user }
 
   before do
-    action.object = tool
-    action.save
+    @action = UserAction.new user: user, event: 'created', object: user
+
+    @action.object = tool
+    @action.save
   end
 
   it 'should have a type' do
-    action.type.should == 'created Tool'
+    @action.type.should == 'created User'
   end
 
   it 'should be attached to a user' do
-    action.user.should == user
+    @action.user.should == user
   end
 
   it 'should have a date and time' do
-    action.created_at.should
+    @action.created_at.should
   end
 
   it 'should have an object_type' do
-      action.obj_type.should == tool.class.to_s
+      @action.obj_type.should == tool.class.to_s
   end
 
   it 'should have an object_id' do
-    action.obj_id.should == tool.id
+    @action.obj_id.should == tool.id
   end
 
   it 'should have an object' do
-    action.object.should == tool
+    @action.object.should == tool
   end
 end
