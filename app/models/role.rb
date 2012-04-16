65bfe06ef6d8350514e2b7520b537d5ff9d6655c
Ryan Rix <ry@n.rix.si>
2012-04-16 15:56:28 -0700

    Fix role<=>permission relation

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,7 @@ class Role
   include Mongoid::Document
 
   has_many :users
-  has_many :permissions
+  has_and_belongs_to_many :permissions
 
   field :name,        :type => String
   field :description, :type => String
