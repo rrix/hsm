3be71e5d05de802d1598317f82968da13ac245f1
Ryan Rix <ry@n.rix.si>
2012-04-16 15:24:53 -0700

    Unbreak controller override

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Hsm::Application.routes.draw do
     resource :settings
   end
 
-  devise_for :users #, :controllers => {:registrations => "registrations"}
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
   match 'users',     :to => 'users#index'
   match 'users/:id', :to => 'users#show', :as => "user"
