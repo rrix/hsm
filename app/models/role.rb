b5fa863ef71fe8b37be88773fdb9e599806c3d69
Ryan Rix <ry@n.rix.si>
2012-04-15 20:48:43 -0700

    Broken relations

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,8 @@
 class Role
   include Mongoid::Document
 
-  has_many :users
+  belongs_to :user 
+  has_many   :users
 
   field :name,        :type => String
   field :description, :type => String
